361f35be664d7cbcfeb32a72d5cef8bba4df29d1
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-11-02 15:35:46 -0800

    this is my initial commit

---
+++ gramCracker/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Lindsey on 11/2/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import Foundation
+
+let kParseAppID = "z8Hwt5kDfDhqm7TLfy6j2by5dhpc2K1w4V0krLFd"
+let kParseClientKey = "kqApVBJTRUHBHPivD840W9iaVYBzBXynvVSesJAg"
